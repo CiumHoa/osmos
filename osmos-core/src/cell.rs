b3c6a048bcbb808aa4e427356836151192707184
尚公子 <84230001+metfairy@users.noreply.github.com>
2023-02-23 13:12:50 +0000

    初始化cell

--- osmos-core/src/cell.rs
+++ osmos-core/src/cell.rs
@@ -5,3 +5,37 @@ pub struct Cell {
     pub energy: usize,
     // pub sensor: crate::sensor::Sensor,
 }
+
+impl Cell {
+    pub fn random(rng: &mut rand::rngs::ThreadRng) -> Self {
+        let random_x = rand::Rng::gen_range(rng, 0.0..=1.0);
+        let random_y = rand::Rng::gen_range(rng, 0.0..=1.0);
+        Self {
+            position: nalgebra::Point2::new(random_x, random_y),
+            acceleration: nalgebra::Vector2::new(0.0, 0.0),
+            velocity: nalgebra::Vector2::new(0.0, 0.0),
+            energy: rand::Rng::gen_range(rng, 1..=2),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    mod random {
+        #[test]
+        fn test() {
+            let mut rng = rand::thread_rng();
+            let cell_list = (0..100)
+                .map(|_| crate::cell::Cell::random(&mut rng))
+                .collect::<Vec<crate::cell::Cell>>();
+            for cell in cell_list {
+                assert!(cell.energy <= 2);
+                assert!(cell.energy >= 1);
+                assert!(cell.position.x >= 0.0);
+                assert!(cell.position.y >= 0.0);
+                assert!(cell.position.x <= 1.0);
+                assert!(cell.position.y <= 1.0);
+            }
+        }
+    }
+}
